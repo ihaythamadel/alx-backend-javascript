0eac3b24cc5672d16aca1753fbfebe3df5730351
ihaythamadel <123040960+ihaythamadel@users.noreply.github.com>
2024-03-12 17:02:03 +0200

    Delete 0x03-ES6_data_manipulation directory

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++
@@ -1,3 +0,0 @@
-export default function getStudentIdsSum(students) {
-  return students.reduce((accumulator, student) => accumulator + student.id, 0);
-}
--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++
@@ -1,3 +0,0 @@
-export default function hasValuesFromArray(_set, array) {
-  return array.map((x) => _set.has(x)).reduce((x, y) => (!!y), false);
-}
